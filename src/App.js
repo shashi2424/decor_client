e0183d717745b6e400e0d87d9ea1dc287b080d1a
Sys2024 <shashikumar@way2news.com>
2023-08-17 14:46:28 +0530

    Design fixed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import GlobalStyles from 'styles/GlobalStyles';
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RestaurantLandingPage from "demos/RestaurantLandingPage";
+
+export default function App() {
+
+
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={<RestaurantLandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+
